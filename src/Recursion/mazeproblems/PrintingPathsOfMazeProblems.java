59e9b62af6a5e05f0a06a331905d3317ef28b255
Satya Sai <satya07thota@gmail.com>
2023-04-20 21:48:59 +0530

    Printing Paths To Maze Problems Through Process/UnProcessed.

--- src/Recursion/mazeproblems/PrintingPathsOfMazeProblems.java
+++ src/Recursion/mazeproblems/PrintingPathsOfMazeProblems.java
@@ -1,9 +1,13 @@
 package Recursion.mazeproblems;
 
+import java.util.ArrayList;
+
+//Maze Problems Means Either We Will Travel The Maze Space To Either Right or Down
 public class PrintingPathsOfMazeProblems {
     public static void main(String[] args) {
             PrintingPathsOfMazeProblems obj=new PrintingPathsOfMazeProblems();
             obj.printingpaths("",3,3);
+            System.out.println(obj.printingPathsList("",3,3));
     }
 
     public void printingpaths(String ans,int row,int column){
@@ -19,5 +23,23 @@ public class PrintingPathsOfMazeProblems {
         if(column>1) {
             printingpaths(ans + "R", row, column - 1);
         }
+        }
+
+        public ArrayList<String> printingPathsList(String ans,int row,int column){
+
+            if(row==1 && column==1){
+                ArrayList<String> list=new ArrayList<>();
+                list.add(ans);
+                return list;
+            }
+
+            ArrayList<String> list=new ArrayList<>();
+            if(row>1) {
+                 list.addAll(printingPathsList(ans + "D", row - 1, column));
+            }
+            if(column>1) {
+                list.addAll(printingPathsList(ans + "R", row, column - 1));
+            }
+            return list;
         }
     }
