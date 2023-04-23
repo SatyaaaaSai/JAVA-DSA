18df2700b0e6ac947d78485674c7ae4341934cc4
Satya Sai <satya07thota@gmail.com>
2023-04-23 13:33:37 +0530

    Usages Of Constructors And SuperClass In Inheritance Concept.

---
+++ src/OOPPrinciples/BoxWeight.java
@@ -0,0 +1,30 @@
+package OOPPrinciples;
+
+public class BoxWeight extends Box{ //Box File Is The Above Box File.Because this is in the package/!\
+    public int weight;
+    BoxWeight(){
+        this.weight=-1;
+    }
+
+    public BoxWeight(int l, int w, int h, int weight) {
+        super(l, w, h);//call The Box Constructor.
+        this.weight = weight;
+    }
+
+    BoxWeight(BoxWeight other){
+        super(other);
+        weight=other.weight;
+    }
+
+    public static void main(String[] args) {
+        BoxWeight obj=new BoxWeight();
+        BoxWeight obj2=new BoxWeight(12,13,14,15);
+        BoxWeight obj3=new BoxWeight(obj2);
+        System.out.println(obj.weight);
+        System.out.println(obj2.l+" "+obj2.w+" "+obj2.h+" " + obj2.weight);
+        System.out.println("Constructor Calling in super class's "+" " +obj3.l+" "+obj3.w+" "+obj3.h+" " + obj3.weight);
+        Box obj4=new BoxWeight(12,12,12,12);
+        System.out.println(obj4.l);
+    }
+
+}
