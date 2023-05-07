0c1a241571a93cceddaa68249e704a6c7fd94958
Satya Sai <satya07thota@gmail.com>
2023-05-07 17:31:06 +0530

    LC::141::Slow And Fast Pointer Method.

--- src/LinkedList/Problems/LinkedListCycle.java
+++ src/LinkedList/Problems/LinkedListCycle.java
@@ -30,4 +30,27 @@ public class LinkedListCycle {
 
             return false;
         }
+
+        //find the length of the cycle
+
+    public int hasCycle(ListNode head) {
+        ListNode fast=head;
+        ListNode slow=head;
+        while(fast!=null && fast.next!=null){
+            fast=fast.next.next; //fast will increments two moves .
+            slow=slow.next; //slow incrememts one time..
+            if(fast==slow){
+               //iterate the loop until slow meets fast..
+                ListNode temp=slow;
+                int length=0;
+                do{
+                    temp=temp.next;
+                    length++;
+                }while(temp!=slow) //temp!=fast..
+            }
+
+        }
+
+        return 0;
+
 }
